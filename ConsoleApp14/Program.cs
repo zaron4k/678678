f521869bea060356715621a79e4242bf1700425a
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-06-05 10:59:04 +0300

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        //enum Month
+        enum DayWeek
+        {
+          // январь, февраль, март, апрель, май, июнь, июль, август, сентябрь, октябрь, ноябрь, декабрь
+          Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Days.Четверг);
+            //    Console.WriteLine((int)Days.Четверг);
+            //Days days = Days.Четверг;
+            //Console.WriteLine(days);
+            //Console.WriteLine((int)days);
+            //foreach(int i in Enum.GetValues(typeof(Days)))
+            //    {
+            //    Console.WriteLine(i);
+            //}
+            //foreach(var day in Enum.GetValues(typeof(Days)))
+            //{
+            //    Console.WriteLine(day.ToString());
+            //}
+            //Days i;
+            //for (i = Days.Sat; i <= Days.Fri; i++)
+            //    Console.WriteLine(i + " имеет значение" + (int)i);
+            //DayWeek D;
+            //D = DayWeek.Sunday;
+            //D++;
+            //Console.WriteLine(D);
+            //D += 2;
+            //Console.WriteLine(D);
+            //D += 10;
+            //Console.WriteLine(D);
+            //D = DayWeek.Friday;
+            //--D;
+            //Console.WriteLine(D);
+            //D = D - 8;
+            //Console.Read();
+            //Console.Read();
+            //Month m;
+            //foreach(var month in Enum.GetValues(typeof(Month)))
+            //{
+            //    Console.WriteLine(month.ToString());
+            //}
+            //Console.Read();
+        }
+    }
+}
